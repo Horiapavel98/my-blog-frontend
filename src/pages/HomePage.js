04c9b53b676ee45cb90b27f04e7af2d82cf3abd9
Horia <32480920+Horiapavel98@users.noreply.github.com>
2021-08-25 11:35:16 +0300

    Initial commit

---
+++ src/pages/HomePage.js
@@ -0,0 +1,30 @@
+import React from "react";
+
+const HomePage = () => (
+  <>
+    <h1>Hello, welcome to my blog!</h1>
+
+    <p>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent
+      efficitur ligula in eros bibendum, id consectetur nunc scelerisque.
+      Curabitur eleifend metus eget interdum faucibus. Ut at tincidunt nunc.
+      Cras vulputate eros id metus molestie, ut dictum ex accumsan. Ut eu
+      pharetra metus, in placerat dolor. Sed sed luctus purus, eget posuere
+      diam. Maecenas a dictum urna. Nulla tristique leo ac viverra placerat.
+    </p>
+
+    <p>
+      Morbi vitae faucibus ante, et placerat enim. Aliquam et ligula id tellus
+      vestibulum consectetur nec ac ipsum. Maecenas sit amet sagittis sapien, eu
+      bibendum elit. Quisque eget quam lorem. In porttitor dictum libero id
+      hendrerit. Vestibulum efficitur eleifend nunc vel imperdiet. Pellentesque
+      pellentesque dui et mi congue, ut congue velit feugiat. Maecenas mattis
+      dolor lectus, vel bibendum purus efficitur sit amet. Sed molestie sem
+      elit, feugiat elementum neque accumsan porttitor. Mauris tempor id ante a
+      egestas. Praesent at luctus nisi. Ut varius elit non diam volutpat
+      efficitur. Phasellus ut arcu vitae lorem rhoncus interdum.
+    </p>
+  </>
+);
+
+export default HomePage;
